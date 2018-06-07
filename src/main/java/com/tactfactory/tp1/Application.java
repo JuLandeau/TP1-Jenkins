126b6513df2f80e5960f0e33ba8576a37a063fda
Julien <j.landeau@iia-laval.fr>
2018-06-07 11:37:50 +0200

    Ajout de pmd

--- src/main/java/com/tactfactory/tp1/Application.java
+++ src/main/java/com/tactfactory/tp1/Application.java
@@ -1,14 +1,31 @@
+/*******************************************************************************
+ * copyright
+ ******************************************************************************/
 package com.tactfactory.tp1;
 
 import org.joda.time.DateTime;
 
+/**
+ * My main Application
+ * @author Julien
+ */
 public class Application {
-	
+	/**
+	 * Enter point application.
+	 * @param args Argument from CLI.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		DateTime date = DateTime.now();
 	}
 	
+	/**
+	 * Add method.
+	 * 
+	 * @param i first value.
+	 * @param j second value.
+	 * @return result of the addition.
+	 */
 	public static int add(int i, int j)
 	{
 		return i + j;
--- src/test/java/com/tactfactory/tp1/ApplicationTest.java
+++ src/test/java/com/tactfactory/tp1/ApplicationTest.java
@@ -1,16 +1,30 @@
+/*******************************************************************************
+ * copyright
+ ******************************************************************************/
 package com.tactfactory.tp1;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ApplicationTest {
 
+	@Before
+	public void setup() {
+		System.out.println("Before");
+	}
+
+	@After
+	public void testDown() {
+		System.out.println("After");
+	}
+
 	@Test
 	public void testAdd() {
 		int i = 2, j = 3;
-		int result = Application.add(i, j); 
+		int result = Application.add(i, j);
 		Assert.assertEquals("Add not good", result, 5);
+		System.out.println("Equals");
 	}
 }
